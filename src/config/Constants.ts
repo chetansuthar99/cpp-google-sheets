db203c49b94a38e23745823baf7d4481d231a5d3
chetansuthar99 <chetansuthar@gmail.com>
2026-08-08 23:24:52 +0530

    Add application-wide constants for CPP

---
+++ src/config/Constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Chetan Portfolio Pro (CPP)
+ * Application-wide constants.
+ */
+
+export const APP_NAME = "Chetan Portfolio Pro";
+export const APP_SHORT_NAME = "CPP";
+export const APP_VERSION = "0.1.0-alpha";
